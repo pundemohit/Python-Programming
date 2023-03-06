ac2c96d5f78799843add2bc8fd7a1b16a7d15883
Mohit <pundemohit@gmail.com>
2023-03-06 09:12:47 +0530

    100 Days Code Challenge

---
+++ commentsAndEscapeSequence.py
@@ -0,0 +1,18 @@
+''' 
+this is multi line comment ->
+'''
+
+#This is a 'Single-Line Comment type anything here :)'
+print("This is a print statement.")
+
+print("hey there") #printing hello world
+#print("This doesnt "execute")
+print("This will \" execute \" ", 5, 6.7, sep='~')
+
+#It will execute a block of code if a specified condition is true.
+#If the condition is false then it will execute another block of code.
+p = 7
+if (p > 5):
+    print("p is greater than 5.")
+else:
+    print("p is not greater than 5.")
---
+++ firstProgram.py
@@ -0,0 +1,5 @@
+print("Hello World", 7)
+print(5)
+print("Bye")
+print(17*13)
+print("Hello moto")
