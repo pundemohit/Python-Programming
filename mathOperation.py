1164ed192604728a311569586f81c967912f4943
Mohit <pundemohit@gmail.com>
2023-03-07 10:26:51 +0530

    100 Days Code Challenge

---
+++ mathOperation.py
@@ -0,0 +1,18 @@
+n1 = 10
+n2 = 3
+
+print("ADDITION of 2 nos", n1, "and", n2, "=", n1+n2)
+
+m = n2
+n = n1
+
+ans2 = n-m
+print("Subtraction of",n,"and",m,"is", ans2)
+ans3 = n*m
+print("Multiplication of",n,"and",m,"is", ans3)
+ans4 = n/m
+print("Division of",n,"and",m,"is", ans4)
+ans5 = n%m
+print("Modulus of",n,"and",m,"is", ans5)
+ans6 = n//m
+print("Floor Division of",n,"and",m,"is", ans6)
