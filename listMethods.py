19d07b62906e97f520ae2e5daba08d2a3f66d047
Mohit <pundemohit@gmail.com>
2023-03-16 10:19:16 +0530

    100 Days Code Challenge

---
+++ listMethods.py
@@ -0,0 +1,28 @@
+l = [11, 45, 1, 2, 4, 6, 1, 1]
+print(l)
+# l.append(7)
+# l.sort(reverse=True)
+# l.reverse()
+# print(l.index(1))
+# print(l.count(1))
+# m = l.copy()
+# m[0] = 0
+# l.insert(1, 899)
+m = [900, 1000, 1100]
+k = l + m
+# print(k)
+# l.extend(m)
+print(l)
+
+colors = ["voilet", "indigo", "blue", "green"]
+colors.reverse()
+print(colors)
+
+num = [4,2,5,3,6,1,2,1,2,8,9,7]
+num.reverse()
+print(num)
+
+
+colors = ["voilet", "green", "indigo", "blue", "green"]
+print(colors.count("green"))
+
